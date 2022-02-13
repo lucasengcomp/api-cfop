9563fa5164495771a9d63859be86e6b2051bd9d1
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-02-13 18:48:48 -0300

    Project created

---
+++ backend/src/main/java/com/br/cfop/ApiCfopApplication.java
@@ -0,0 +1,13 @@
+package com.br.cfop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCfopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCfopApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/br/cfop/ApiCfopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cfop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCfopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
